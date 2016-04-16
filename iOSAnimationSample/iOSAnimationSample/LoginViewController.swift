42f39cad0630cd52b561779fc003403e6fec8110
ArronYR <yangyun4814@gmail.com>
2016-04-16 18:00:36 +0800

    finish login animation

--- iOSAnimationSample/iOSAnimationSample/LoginViewController.swift
+++ iOSAnimationSample/iOSAnimationSample/LoginViewController.swift
@@ -17,6 +17,14 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var bubble5: UIImageView!
     @IBOutlet weak var logo: UIImageView!
     @IBOutlet weak var dot: UIImageView!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var warning: UIImageView!
+    
+    var loginPosition = CGPoint.zero
+    //spinner
+    let spinner = UIActivityIndicatorView(activityIndicatorStyle: .WhiteLarge)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +37,32 @@ class LoginViewController: UIViewController {
         
         self.dot.center.x -= self.view.bounds.width/3
         self.logo.center.x -= self.view.bounds.width
+        
+        // adjust place holder icon
+        let usernamePaddingLeft = UIView(frame: CGRectMake(0,0,44, self.username.frame.height))
+        self.username.leftView = usernamePaddingLeft
+        self.username.leftViewMode = .Always
+        
+        let passwordPaddingLeft = UIView(frame: CGRectMake(0,0,44, self.password.frame.height))
+        self.password.leftView = passwordPaddingLeft
+        self.password.leftViewMode = .Always
+        
+        let userImageView = UIImageView(image: UIImage(named: "user"))
+        userImageView.frame.origin = CGPoint(x: 13, y: 9)
+        self.username.addSubview(userImageView)
+        
+        let keyImageView = UIImageView(image: UIImage(named: "key"))
+        keyImageView.frame.origin = CGPoint(x: 12, y: 9)
+        self.password.addSubview(keyImageView)
+        
+        self.username.center.x -= self.view.bounds.width
+        self.password.center.x -= self.view.bounds.width
+        
+        self.loginPosition = self.loginButton.center
+        self.loginButton.center.x -= self.view.bounds.width
+        
+        self.warning.center = self.loginPosition
+        self.warning.layer.cornerRadius = 5
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,5 +91,43 @@ class LoginViewController: UIViewController {
         UIView.animateWithDuration(5, delay: 0.4, usingSpringWithDamping: 0.1, initialSpringVelocity: 1, options: [], animations: { () -> Void in
             self.dot.center.x += self.view.bounds.width/3
         }, completion: nil)
+        
+        UIView.animateWithDuration(0.4, delay: 0.5, options: [.CurveEaseOut], animations: { () -> Void in
+            self.username.center.x += self.view.bounds.width
+        }, completion: nil)
+        UIView.animateWithDuration(0.4, delay: 0.6, options: [.CurveEaseOut], animations: { () -> Void in
+            self.password.center.x += self.view.bounds.width
+        }, completion: nil)
+        
+        UIView.animateWithDuration(0.4, delay: 0.7, options: [.CurveEaseOut], animations: { () -> Void in
+            self.loginButton.center.x += self.view.bounds.width
+        }, completion: nil)
+    }
+    
+    @IBAction func okLoginTapped(sender: AnyObject) {
+        self.spinner.frame.origin = CGPointMake(12, 10)
+        self.loginButton.addSubview(spinner)
+        self.spinner.startAnimating()
+        
+        UIView.transitionWithView(self.warning, duration: 0.3, options: [.TransitionFlipFromBottom], animations: { () -> Void in
+            self.warning.hidden = true
+        }, completion: nil)
+        
+        UIView.animateWithDuration(0.3) { () -> Void in
+            self.loginButton.center = self.loginPosition
+        }
+        self.loginButton.center.x -= 30
+        UIView.animateWithDuration(1.5, delay: 0, usingSpringWithDamping: 0.1, initialSpringVelocity: 0, options: [], animations: { () -> Void in
+            self.loginButton.center.x += 30
+        }, completion: { _ in
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                self.loginButton.center.y += 80
+                self.spinner.removeFromSuperview()
+            }, completion: { _ in
+                UIView.transitionWithView(self.warning, duration: 0.3, options: [.TransitionFlipFromTop, .CurveEaseInOut], animations: { () -> Void in
+                    self.warning.hidden = false
+                }, completion: nil)
+            })
+        })
     }
 }
